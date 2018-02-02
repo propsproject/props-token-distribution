3b0a8c1e8be753f56f5ecdb3d44bb938b661e27b
Ludovic Galabru <ludovic@galabru.com>
2018-02-02 14:45:09 -0500

    Cleaning test

--- test/PropsToken.js
+++ test/PropsToken.js
@@ -12,13 +12,21 @@ const advanceBlock = helpers.advanceBlock;
 
 const PropsToken = artifacts.require('./helpers/PropsTokenImpl.sol')
 
+const formattedAddress = (address) => {
+  return  Buffer.from(ethUtil.stripHexPrefix(address), 'hex');
+};
+const formattedInt = (int) => {
+  return ethUtil.setLengthLeft(int, 32);
+};
+const formattedBytes32 = (bytes) => {
+  return ethUtil.addHexPrefix(bytes.toString('hex'));
+};
+
 contract('PropsToken', ([
-  _,
-  owner,
-  attacker,
   alice,
   bob,
-  charlie
+  charlie,
+  owner
 ]) => {
 
   before(async () => {
@@ -35,44 +43,30 @@ contract('PropsToken', ([
     });
 
     it('Charlie performs a transfer of 100 tokens from Alice to Bob, taking 10 in fees', async () => {
-      const fee = 10;
-      const amount = 100;
-      const token = this.token.address;
+      const nonce = 32;
       const from = alice;
       const to = bob;
       const delegate = charlie;
-      const nonce = 32;
-
-      const bufferedAddress = (address) => {
-        return  Buffer.from(ethUtil.stripHexPrefix(address), 'hex');
-      };
-      const bufferedInt = (int) => {
-        return ethUtil.setLengthLeft(int, 32);
-      };
-      const formattedByte32 = (bytes) => {
-        return ethUtil.addHexPrefix(bytes.toString('hex'));
-      };
+      const fee = 10;
+      const amount = 100;
+      const alicePrivateKey = Buffer.from('c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3', 'hex');
 
       const components = [
-        bufferedAddress(delegate),
-        bufferedAddress(token),
-        bufferedInt(nonce),
-        bufferedAddress(from),
-        bufferedAddress(to),
-        bufferedInt(amount),
-        bufferedInt(fee)
+        Buffer.from('6e74f5d1', 'hex'),
+        formattedAddress(this.token.address),
+        formattedInt(nonce),
+        formattedAddress(from),
+        formattedAddress(to),
+        formattedAddress(delegate),
+        formattedInt(amount),
+        formattedInt(fee)
       ];
 
       const tightPack = Buffer.concat(components);
 
       const hashedTightPack = ethUtil.sha3(tightPack);
 
-      const alicePrivateKey = Buffer.from('c88b703fb08cbea894b6aeff5a544fb92e78a18e19814cd85da83b71f772aa6c', 'hex');
-
       const sig = ethUtil.ecsign(hashedTightPack, alicePrivateKey)
-
-      const pubkey = ethUtil.ecrecover(hashedTightPack, sig.v, sig.r, sig.s)
-      const address = ethUtil.publicToAddress(pubkey)
 
       const tx = await this.token.delegatedTransfer(
         nonce,
@@ -81,11 +75,34 @@ contract('PropsToken', ([
         amount,
         fee,
         sig.v,
-        formattedByte32(sig.r),
-        formattedByte32(sig.s), {from: charlie});
-
-      console.log(tx.logs[0].args);
+        formattedBytes32(sig.r),
+        formattedBytes32(sig.s), {from: charlie});
     });
   });
-
 });
+
+
+/*
+const fee = 10;
+const amount = 100;
+const token = this.token.address;
+const from = alice;
+const to = bob;
+const delegate = charlie;
+const nonce = 32;
+const alicePrivateKey = Buffer.from('c88b703fb08cbea894b6aeff5a544fb92e78a18e19814cd85da83b71f772aa6c', 'hex');
+
+const tightPack = (await this.token.delegatedTransferHash.request(token, nonce, from, to, delegate, amount, fee)).params[0].data;
+const hashedTightPack = ethUtil.sha3(tightPack);
+const sig = ethUtil.ecsign(hashedTightPack, alicePrivateKey)
+const tx = await this.token.delegatedTransfer(
+  nonce,
+  from,
+  to,
+  amount,
+  fee,
+  sig.v,
+  ethUtil.addHexPrefix(sig.r.toString('hex')),
+  ethUtil.addHexPrefix(sig.s.toString('hex')),
+  {from: charlie});
+*/
