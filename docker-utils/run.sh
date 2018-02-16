e4cbba908dc67c7236ce87ba058ab0e9a5a036af
Khalil Claybon <kclaybon1@gmail.com>
2018-02-16 12:15:52 -0500

    add ability to run docker container with ganache-cli and token deployed, express server to get info

---
+++ docker-utils/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+errNumArgs="Illegal number of arguments. Please provide 3 different accounts private keys"
+if [ "$#" -ne 3 ]; then
+    echo $errNumArgs
+    exit 1
+fi
+
+ganachePID=$(ps aux | pgrep 'ganache-cli -p 9545' | awk '{print $2}')
+if [[ $ganachePID ]]; then
+    kill $ganachePID
+fi
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+owner=$1
+sender=$2
+recipient=$3
+balance=$(( 10 ** 20 ))
+
+nohup ganache-cli -p 9000 -i 8000 --account="$owner, $balance" --account="$sender, $balance" --account="$recipient, $balance" &
+truffle exec ./deploy.js --network docker
+node ./server.js 
