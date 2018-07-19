64dbefae75ae3b75b62d165031f3f46b131ee625
jon <jon@younow.com>
2018-07-19 05:30:42 -0400

    ready for execution

--- contracts/token/PropsToken.sol
+++ contracts/token/PropsToken.sol
@@ -23,10 +23,10 @@ import "./ERC865Token.sol";
 
 contract PropsToken is ERC865Token, PausableToken, MintableToken {
 
-    /* Set the token name for display */
+    /* Set the token symbol for display */
     string public constant symbol = "TOKEN";
 
-    /* Set the token symbol for display */
+    /* Set the token name for display */
     string public constant name = "Token";
 
     /* Set the number of decimals for display */
